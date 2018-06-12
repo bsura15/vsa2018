b2efa1b91b44cb0eae62b971cf124959121fd6b2
bsura15 <40149414+bsura15@users.noreply.github.com>
2018-06-12 09:56:47 -0500

    Add files via upload

--- proj01_ifelse/proj01.py
+++ proj01_ifelse/proj01.py
@@ -33,10 +33,24 @@
 # Then, it prints a sentence that says the number of days and months until their birthday
 current_day= 11
 current_month= 6
-month= raw_input("what is your birth month?")
-print month
-day= raw_input("what is your birth day?")
-print day
+birth_month= int(raw_input("what is your birth month in a number?"))
+print birth_month
+birth_day= int(raw_input("what is the day your were born?(number)"))
+print "day"
+if birth_month > current_month:
+    print "your birthday is in" + str(birth_month - current_month)
+else:
+    print "your birthday is in" + str(12-(current_month - birth_month))
+print "months"
+if birth_day > current_day:
+    print "your birthday is in" + str(birth_day - current_day)
+else:
+    print "your birthday is in" + str(30-(current_day - birth_day))
+print "days"
+
+
+
+
 
 
 
