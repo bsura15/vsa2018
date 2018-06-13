ee2a0df2018a6960538e222d2a8420db9c433206
bsura15 <40149414+bsura15@users.noreply.github.com>
2018-06-13 10:03:01 -0500

    Add files via upload

--- proj03_guessing_game/proj03.py
+++ proj03_guessing_game/proj03.py
@@ -13,3 +13,29 @@ Keep track of how many guesses the user has taken, and when the game ends, print
 """
 
 
+import random
+variable_number = random.randint(1, 9)
+user_input = raw_input("Im thinking of a number between 1 and 9. There is a limitation of 5 guesses. Can you guess my number? Enter a number, or 'exit' to end the game:")
+
+
+sum = 1
+
+
+
+while user_input != str("exit") and sum < 5:
+    if int(user_input) > variable_number:
+        print "Your number is too high!"
+        sum = sum + 1
+        user_input = raw_input("Enter a number, or 'exit' to end the game.")
+    elif int(user_input) < variable_number:
+        print "Your number is too low!"
+        sum = sum + 1
+        user_input = raw_input("Try again.")
+    elif int(user_input) == variable_number:
+        print "Congratulations, you guessed my number!"
+        print str("You used " + str(sum) + str(" guess(es)."))
+        break
+
+
+
+
