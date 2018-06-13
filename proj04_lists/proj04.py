3ee95511f8f7ce4b731268da2aa91e85c2a82962
bsura15 <40149414+bsura15@users.noreply.github.com>
2018-06-13 14:35:42 -0500

    Add files via upload

--- proj04_lists/proj04.py
+++ proj04_lists/proj04.py
@@ -12,12 +12,16 @@ practice with lists
 #Part I
 #Take a list, say for example this one:
 
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+numb_name = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 #and write a program that prints out all the elements of the list that are less than 5.
 
+for item in numb_name:
+    if item < 5:
+        print item
 
 
 
+print "______"
 
 
 #Part II
@@ -27,6 +31,14 @@ c = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 # and write a program that creates and prints a list that contains only the elements
 # that are common between the lists (without duplicates).
 # Make sure your program works on two lists of different sizes.
+
+a = [11, 22 ,3 ,6 , 8 , 9 ,15 ,10 ,12 ,4]
+b = [88, 53 ,3 ,5 , 8, 7, 15, 34, 14, 2]
+
+for item in a:
+    for item_b in b:
+        if item == item_b:
+            print item
 
 
 
@@ -38,19 +50,28 @@ c = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 d = ["b", "a", "f", "y", "a", "t", "_", "p", "a", "R"]
 # and write a program that replaces all “a” with “*”.
 
+counter = 0
+#sum = 1
+for item in d:
+    if item == "a":
+        d[counter] = "*"
+    counter = counter + 1
+print d
 
 
-
-
-
-
-
+print "________"
 
 #Part IV
+
 #Ask the user for a string, and print out whether this string is a palindrome or not.
 
-
-
+print "Hello!"
+list_one = str
+list_two = str
+str = raw_input("Choose a word and I will tell you whether it is a palindrome or not:")
+list_two.reverse
+list_one = list_two
+print ""
 
 
 
